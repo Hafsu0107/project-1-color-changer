bed4a42a351264766a7fe6f76a9388368544aca1
Hafsa600-dot <princehafsa156@gmail.com>
2024-11-26 16:13:03 +0500

    js complete

--- color.js
+++ color.js
@@ -1,8 +1,8 @@
-const buttons = document.querySelector(".colors");
+const colors = document.querySelectorAll(".colors");
 const body = document.querySelector("body");
 colors.forEach(function (color) {
-  console.log(button);
-  colors.addEvenListner("click", function (e) {
+  console.log(colors);
+  color.addEventListener("click", function (e) {
     console.log(e);
     console.log(e.target);
     if (e.target.id === "355C7D") {
@@ -11,10 +11,10 @@ colors.forEach(function (color) {
     if (e.target.id === "725A7A") {
       body.style.backgroundColor = e.target.id;
     }
-    if (e.target.id === "yellow") {
+    if (e.target.id === "C56C86") {
       body.style.backgroundColor = e.target.id;
     }
-    if (e.target.id === "green") {
+    if (e.target.id === "FF7582") {
       body.style.backgroundColor = e.target.id;
     }
   });
